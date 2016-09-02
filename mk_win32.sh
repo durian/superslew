d1f46d51143e69af528761c4c958df930595eaaa
Peter Berck <peter@berck.se>
2016-09-02 16:59:35 +0200

    Fixes in script.

--- mk_win32.sh
+++ mk_win32.sh
@@ -2,3 +2,4 @@ mkdir -p build32
 cd build32
 cmake -G "Visual Studio 12" ..
 cmake --build . --config Release
+echo scp.exe bin/superslew/32/Release/win.xpl pberck@192.168.1.66:dropbox/BIN/superslew/32/
--- mk_win64.sh
+++ mk_win64.sh
@@ -2,3 +2,4 @@ mkdir -p build
 cd build
 cmake -G "Visual Studio 12 Win64" ..
 cmake --build . --config Release
+echo scp.exe bin/superslew/64/Release/win.xpl pberck@192.168.1.66:dropbox/BIN/superslew/64/
