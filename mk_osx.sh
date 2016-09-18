e69b5fd46f20f5362d09b57aa55f165dcf68df23
Peter Berck <peter@berck.se>
2016-09-18 08:43:24 +0200

    Needed after XCode 8 update.

--- mk_osx.sh
+++ mk_osx.sh
@@ -1,7 +1,8 @@
 BUILD="build"
 mkdir -p ${BUILD}
 cd ${BUILD}
-cmake ..
+#https://github.com/Homebrew/legacy-homebrew/issues/46362
+cmake -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk ..
 make
 cd ..
 echo cp bin/superslew/mac.xpl ~/xplanec/Resources/plugins/groundcontrol/
